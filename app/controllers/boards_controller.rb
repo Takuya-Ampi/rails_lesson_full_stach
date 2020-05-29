5176228323d4f7904dee5e3bcb64fdbbfc4f82f5
行比拓也 <imac@iMac.local>
2020-05-29 22:40:04 +0900

    掲示板データの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,4 +4,14 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+  def create
+    Board.create(board_params)
+  end
+
+  # ストロングパラメーター。意図していないparamsが渡ってくるのを防ぐ。フィルターをかけている
+  private
+  def board_params
+    # paramsの中にある、boardキーのname,title,bodyの値のみ受け取る
+    params.require(:board).permit(:name, :title, :body)
+  end
 end
